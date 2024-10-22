242dac7462432394ccda0be795f13a80acdfafba
Gamaliel Adun <adungamaliel@gmail.com>
2024-10-22 14:41:37 +0100

    updated binary tree implementation and added tests

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
 import { Tree, prettyPrint } from "./bst.mjs";
 
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+function generateRandomArray(length, min, max) {
+  const inputArray = [];
 
-// console.log(prettyPrint(tree.root));
-tree.insert(3.5);
-tree.insert(7.5);
-tree.insert(40);
-tree.insert(6700);
-tree.insert(-1);
-tree.insert(0);
-tree.insert(7.6);
-tree.insert(4.1);
-tree.insert(4.2);
-tree.insert(4.3);
-// prettyPrint(tree.root);
-// console.log(tree.find(1));
-// console.log(tree.find(0));
-tree.deleteItem(3.5);
-tree.deleteItem(5);
-tree.deleteItem(-1);
-// tree.deleteItem(67);
-// tree.levelOrder((node) => console.log(node.data));
-prettyPrint(tree.root);
-// tree.inOrder((node) => console.log(node.data));
-// tree.preOrder((node) => console.log(node.data));
-// tree.postOrder((node) => console.log(node.data));
-// console.log(prettyPrint(tree.root));
-// console.log(tree.depth(tree.find(23)));
+  for (let i = 0; i < length; i++) {
+    const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+    inputArray.push(randomNumber);
+  }
+  return inputArray;
+}
+
+const treeArray = generateRandomArray(99, 0, 100);
+
+const tree = new Tree(treeArray);
+
 console.log(tree.isBalanced());
-// tree.deleteItem(-1);
+prettyPrint(tree.root);
+
+tree.inOrder((node) => console.log(node.data));
+tree.preOrder((node) => console.log(node.data));
+tree.postOrder((node) => console.log(node.data));
+
+const newTreeArray = generateRandomArray(20, 100, 200);
+
+newTreeArray.map((item) => {
+  tree.insert(item);
+});
+
+console.log(tree.isBalanced());
+prettyPrint(tree.root);
+
 tree.rebalance();
-prettyPrint(tree.root);
 console.log(tree.isBalanced());
+prettyPrint(tree.root);
+
+tree.inOrder((node) => console.log(node.data));
+tree.preOrder((node) => console.log(node.data));
+tree.postOrder((node) => console.log(node.data));
